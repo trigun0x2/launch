692561414fc8eb2431c853cafef244da4b1ac750
Jeffrey Tong <trigun0x2@gmail.com>
2015-01-05 15:51:05 -0500

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,37 @@
+
+/***********************************************************************************
+  
+Animations
+  
+************************************************************************************/
+
+jQuery(document).ready(function($) {
+	$(function() {
+		$('#header').delay(100).fadeIn(1000);
+		$('#intro').delay(300).fadeIn(500);
+	});
+});
+
+/***********************************************************************************
+  
+User Schedule
+  
+************************************************************************************/
+
+jQuery(document).ready(function($) {
+    var easter_egg = new Konami();
+    easter_egg.code = function() { 
+        alert("Congrats on making it to the Challenger's League!")
+        $('#player').slideDown(); 
+    }
+    easter_egg.load();
+
+    $("#code").submit(function (event) {
+        event.preventDefault();
+        code = $("#codebox").val();
+        if (code == "QQZW") {
+            $(".schedule").slideDown();
+            $(".player-groups").fadeOut();
+        }
+    })
+});
